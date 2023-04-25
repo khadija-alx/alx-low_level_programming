f5dd118abf807b4f17f21ecf715538af4589e4b6
khadija-alx <elamri.khadija@gmail.com>
2023-04-24 19:48:50 -0700

    comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,18 +7,18 @@
   */
 int main(void)
 {
-	int i;
+	int n;
 
-	for (i = 48; i <= 57; i++)
+	for (n = 48; n <= 57; n++)
 	{
-		putchar(i);
-		if (i == 57)
+		putchar(n);
+		if (n == 57)
 		{
 			continue;
 		}
 		putchar(',');
 		putchar(',');
 	}
-	putchar('/n');
+	putchar('\n');
 	return (0);
 }
