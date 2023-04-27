b0a7a1d3bdc38a99320c67c0053973ec2ae43972
khadija-alx <elamri.khadija@gmail.com>
2023-04-26 17:17:16 -0700

    last digital

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,10 +1,8 @@
 #include "main.h"
 /**
-* print_last_digit - prints the last digit of a number.
+* print_last_digit - prints the last digit of a number l
 *
 * @n: integer
-*
-* @l: last digit
 *
 * Return: last digit
  */
@@ -16,7 +14,7 @@ int print_last_digit(int n)
 	l = n % 10;
 	if (l < 0)
 	{
-		l = l * -1;
+	l = l * -1;
 	}
 	_putchar(l + '0');
 	return (l);
