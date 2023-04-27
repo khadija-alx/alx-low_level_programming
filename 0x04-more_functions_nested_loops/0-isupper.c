e8d02c586c48603165f1572302a195a3ce1c0a39
khadija-alx <elamri.khadija@gmail.com>
2023-04-27 10:31:30 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int _isuppear(int c)
+{
+	if (isupper(c))
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+
+int _isupper(int c)
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+#endif /* MAIN_H */
