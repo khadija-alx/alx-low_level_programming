aa0e32327630a8c226bd69c2e583783c054c7ecd
khadija-alx <elamri.khadija@gmail.com>
2023-04-28 06:22:39 -0700

    print

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,17 +1,14 @@
 #include "main.h"
 /**
-  * print_most_numbers - prints the numbers
-  *
-  * @i: numbers
+  * print_most_numbers - prints the numbers i
   *
   * Return: void
   */
 
 void print_most_numbers(void)
 {
-	int i = 0;
 
-	for (; i <= 9; i++)
+	for (i = 0; i <= 9; i++)
 	{
 		if (i == 2 || i == 4)
 		{
