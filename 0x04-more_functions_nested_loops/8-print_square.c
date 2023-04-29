2d616b84c40d0dd42399e05dba0f7141efd7dfde
khadija-alx <elamri.khadija@gmail.com>
2023-04-28 19:01:51 -0700

    prime factory

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,20 +1,24 @@
 #include "main.h"
 
+
 /**
-  * print_square - function that prints a square
-  * @size: size
-  * Return: always 0 (success)
-  */
+ * print_square - prints hashes squares.
+ * @size: size of the square.
+ * Return: no return.
+ */
 
 void print_square(int size)
 
 {
 	int i, j;
-
-	for (i = 1; i <= size; i++)
+	for (i = 0; i < size; i++)
 	{
-		for (j = 1; j <= size; j++)
-			_putchar('#');
-		_putchar('\n');
+		for (j = 0; j < size; j++)
+		{
+			_putchar(35);
+		}
+		if (i != size - 1)
+			_putchar('\n');
 	}
+	_putchar('\n');
 }
