a63c6ca9eb5af922343faacd2084b20384adf72f
khadija-alx <elamri.khadija@gmail.com>
2023-04-28 17:15:30 -0700

    diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,20 +9,20 @@
 void print_diagonal(int n)
 
 {
+	int i, j;
+
 	if (n <= 0)
-	{
 		_putchar('\n');
-	}
 	else
 	{
-		int i, j;
-
-		for (i = 1; i < n; i++)
+		for (i = 1; i <= n; i++)
 		{
 			for (j = 1; j < i; j++)
+			{
 				_putchar(' ');
-			_putchar (92);
+			_putchar ('\\');
 			_putchar('\n');
+			}
 		}
 	}
 }
