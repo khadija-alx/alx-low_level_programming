0314170889d6943538efaea85550d03605e3db46
khadija-alx <elamri.khadija@gmail.com>
2023-04-28 18:07:04 -0700

    triangle

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -3,7 +3,7 @@
 /**
   * print_square - function that prints a square
   * @size: size
-  * Return: square
+  * Return: always 0 (success)
   */
 
 void print_square(int size)
@@ -11,9 +11,9 @@ void print_square(int size)
 {
 	int i, j;
 
-	for (i = 0; i < size; i++)
+	for (i = 1; i <= size; i++)
 	{
-		for (j = 0; j < size; j++)
+		for (j = 1; j <= size; j++)
 			_putchar('#');
 		_putchar('\n');
 	}
