d0d90d10b95fe8029057161465117401fa46f4ec
khadija-alx <elamri.khadija@gmail.com>
2023-06-16 09:57:37 -0700

    file;

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,19 +10,17 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *buf;
-	ssize_t fd;
-	ssize_t w;
-	ssize_t t;
+	int fd;
+	char buf[READ_BUF_SIZE * 8];
+	ssize_t bytes;
 
-	fd = open(filename, O_RDONLY);
+	if (!filename || !letters)
+		return (0);
+	fd = open(filname, O_RDONLY);
 	if (fd == -1)
 		return (0);
-	buf = malloc(sizeof(char) * letters);
-	t = read(fd, buf, letters);
-	w = write(STDOUT_FILENO, buf, t);
-
-	free(buf);
+	bytes = read(fd, &buf[0], letters);
+	bytes = write(STDOUT_FILENO, &buf[0], bytes);
 	close(fd);
 	return (w);
 }
