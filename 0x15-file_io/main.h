b4a75faba34eeffb44f87d4bf9394167e8233196
khadija-alx <elamri.khadija@gmail.com>
2023-06-16 14:15:48 -0700

    create file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,5 +1,3 @@
-main.h
-
 #ifndef MAIN_H
 #define MAIN_H
 
@@ -8,6 +6,7 @@ main.h
 #include <fcntl.h>
 #include <unistd.h>
 
+int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
